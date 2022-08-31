cca034b61148b4daeb997bc7a59a2e3102fa372c
fergodani <ferreiragomezdaniel@gmail.com>
2022-08-31 12:03:05 +0200

    Added footer

---
+++ webapp/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer(props){
+
+    return (
+        <div id="footer">
+            <p>Made by Daniel Ferreira Gómez</p>
+        </div>
+    );
+}
